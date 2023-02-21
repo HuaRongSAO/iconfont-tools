012244435fc4fb81d84d6c43b768a3d690c9e01b
Aziz-pang <zhiming3399@gmail.com>
2023-02-21 11:05:22 +0800

    feat: support iconpark remote url

--- lib/interface.d.ts
+++ lib/interface.d.ts
@@ -1,17 +1,17 @@
 export interface Svg {
-  name: string
-  svg: string
+    name: string;
+    svg: string;
 }
 export interface Icon {
-  name: string
-  icon: string
+    name: string;
+    icon: string;
 }
 export interface OPT {
-  iconfontUrl: string
-  path: string
-  dirName: string
-  fileName: string
-  icon: string
-  fontSize: string
-  component: boolean
+    iconfontUrl: string;
+    path: string;
+    dirName: string;
+    fileName: string;
+    icon: string;
+    fontSize: string;
+    component: boolean;
 }
